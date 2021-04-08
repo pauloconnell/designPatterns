7e4809b4d90d28ced33337b32e0dc07c74b1ef7f
Glitch (design-paterns) <none>
2021-04-08 20:40:36 +0000

    🏍⛄️ Updated with Glitch

--- app/components/Creational/Factory.jsx.jsx
+++ app/components/Creational/Factory.jsx.jsx
@@ -16,21 +16,20 @@ const componentsMade = [
 ];
 
 /* the main page for the index route of this app */
-const HelloWorld = function() {
+const Factory = function() {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <h1>Factory Design Pattern</h1>
 
-      <p>This is a starter <a href="http://glitch.com">Glitch</a> app for React! It uses 
-        only a few dependencies to get you started on working with React:</p>
+      <p>This is a starter <a href="http://glitch.com">Glitch</a> app for React! </p>
 
       <UnorderedList items={dependenciesArray} />
 
-      <p>Look in <code>app/components/</code> for two example components:</p>
+      <p> <code></code> </p>
 
       <UnorderedList items={componentsMade} />
     </div>
   );
 }
 
-module.exports = HelloWorld;
+module.exports = Factory;
--- app/components/MainApp.jsx
+++ app/components/MainApp.jsx
@@ -1,12 +1,12 @@
 const React = require("react");
 const UnorderedList = require("./UnorderedList");
-const Set = require("./DataStructures/Set");
-const Array = require("./DataStructures/Array");
-const LinkedList = require("./DataStructures/LinkedList");
-const DynamicProgramming = require("./Algorithms/DynamicProgramming");
-const RandomPointInCircle = require("./CodingChallenges/RandomPointInCircle");
-const DepthFirstSearch = require("./Algorithms/DepthFirstSearch");
-const BreadthFirstSearch = require("./Algorithms/BreadthFirstSearch");
+//const Set = require("./DataStructures/Set");
+//const Array = require("./DataStructures/Array");
+//const LinkedList = require("./DataStructures/LinkedList");
+//const DynamicProgramming = require("./Algorithms/DynamicProgramming");
+//const RandomPointInCircle = require("./CodingChallenges/RandomPointInCircle");
+//const DepthFirstSearch = require("./Algorithms/DepthFirstSearch");
+//const BreadthFirstSearch = require("./Algorithms/BreadthFirstSearch");
 const styles = require("../index.css");
 /* the main page for the index route of this app */
 class MainApp extends React.Component {
@@ -55,86 +55,50 @@ class MainApp extends React.Component {
           <h2> React Components added for each: </h2>
         </center>
         <div style={{ backgroundColor: "lightBlue" }}>
-          Design Pattern Categorization
-
-Design patterns can be categorized in multiple ways, but the most popular one is the following:
-
-    Creational design patterns
-    Structural design patterns
-    Behavioral design patterns
-    Concurrency design patterns
-    Architectural design patterns
-
+          Design Pattern Categorization Design patterns can be categorized in
+          multiple ways, but the most popular one is the following:<br />
+          'Creational design patterns'<br />
+          'Structural design patterns',<br />
+          'Behavioral design patterns',<br />
+          'Concurrency design patterns',<br />
+          'Architectural design patterns'
         </div>
         <div style={{ backgroundColor: "green" }}>
-
-Creational Design Patterns
-
-These patterns deal with object creation mechanisms which optimize object creation compared to a basic approach. The basic form of object creation could result in design problems or in added complexity to the design. Creational design patterns solve this problem by somehow controlling object creation. Some of the popular design patterns in this category are:
-
-    Factory method
-    Abstract factory
-    Builder
-    Prototype
-    Singleton
-
-
+          Creational Design Patterns <br />These patterns deal with object creation
+          mechanisms which optimize object creation compared to a basic
+          approach. The basic form of object creation could result in design
+          problems or in added complexity to the design. Creational design
+          patterns solve this problem by somehow controlling object creation.
+          Some of the popular design patterns in this category are: Factory
+          method Abstract factory Builder Prototype Singleton
         </div>
         <div className="orange">
-Structural Design Patterns
-
-These patterns deal with object relationships. They ensure that if one part of a system changes, the entire system doesn’t need to change along with it. The most popular patterns in this category are:
-
-    Adapter
-    Bridge
-    Composite
-    Decorator
-    Facade
-    Flyweight
-    Proxy
-
-
+          Structural Design Patterns <br />These patterns deal with object
+          relationships. They ensure that if one part of a system changes, the
+          entire system doesn’t need to change along with it. The most popular
+          patterns in this category are: Adapter Bridge Composite Decorator
+          Facade Flyweight Proxy
         </div>
         <div className="blue">
-        Behavioral Design Patterns
-
-These types of patterns recognize, implement, and improve communication between disparate objects in a system. They help ensure that disparate parts of a system have synchronized information. Popular examples of these patterns are:
-
-    Chain of responsibility
-    Command
-    Iterator
-    Mediator
-    Memento
-    Observer
-    State
-    Strategy
-    Visitor
-
-
+          Behavioral Design Patterns <br />These types of patterns recognize,
+          implement, and improve communication between disparate objects in a
+          system. They help ensure that disparate parts of a system have
+          synchronized information. Popular examples of these patterns are:
+          Chain of responsibility Command Iterator Mediator Memento Observer
+          State Strategy Visitor
         </div>
-      <div className="green">
-        Concurrency Design Patterns
-
-These types of design patterns deal with multi-threaded programming paradigms. Some of the popular ones are:
-
-    Active object
-    Nuclear reaction
-    Scheduler
-
-
-        </div>        
+        <div className="green">
+          Concurrency Design Patterns<br /> These types of design patterns deal with
+          multi-threaded programming paradigms. Some of the popular ones are:
+          Active object Nuclear reaction Scheduler
+        </div>
 
         <div className="orange">
-        Architectural Design Patterns
-
-Design patterns which are used for architectural purposes. Some of the most famous ones are:
-
-    MVC (Model-View-Controller)
-    MVP (Model-View-Presenter)
-    MVVM (Model-View-ViewModel)
-
+          Architectural Design Patterns <br />Design patterns which are used for
+          architectural purposes. Some of the most famous ones are: MVC
+          (Model-View-Controller) MVP (Model-View-Presenter) MVVM
+          (Model-View-ViewModel)
         </div>
-        
       </div>
     );
   }
