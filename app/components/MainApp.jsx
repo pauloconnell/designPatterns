a005da0398f3d2f793272649640e25341471dcef
Glitch (design-paterns) <none>
2021-04-25 20:38:01 +0000

    Mediator Design Pattern

--- app/components/MainApp.jsx
+++ app/components/MainApp.jsx
@@ -2,10 +2,11 @@ const React = require("react");
 const UnorderedList = require("./UnorderedList");
 const Singleton = require("./Creational/Singleton");
 const Observer = require("./Behavioral/Observer");
+const Mediator = require("./Behavioral/Mediator");
 const Prototype = require("./Creational/Prototype");
 const Constructor = require("./Creational/Constructor");
-const Module= require("./Creational/Module");
-//const DepthFirstSearch = require("./Algorithms/DepthFirstSearch");
+const Module = require("./Creational/Module");
+
 //const BreadthFirstSearch = require("./Algorithms/BreadthFirstSearch");
 const styles = require("../index.css");
 /* the main page for the index route of this app */
@@ -22,7 +23,6 @@ class MainApp extends React.Component {
     this.handleArray = this.handleArray.bind(this);
     this.handleLinkedList = this.handleLinkedList.bind(this);
     this.handleDp = this.handleDp.bind(this);
-    
   }
 
   componentDidMount() {}
@@ -76,10 +76,18 @@ class MainApp extends React.Component {
           creation could result in design problems or in added complexity to the
           design. Creational design patterns solve this problem by somehow
           controlling object creation. Some of the popular design patterns in
-          this category are: <br/>Factory method<br/> Abstract<br/> factory<br/> Builder<br/> Prototype<br/>
+          this category are: <br />
+          Factory method
+          <br /> Abstract
+          <br /> factory
+          <br /> Builder
+          <br /> Prototype
+          <br />
           Singleton
-          <Constructor showDp={this.state.showDp}
-            handleDp={e => this.handleDp(e)} />
+          <Constructor
+            showDp={this.state.showDp}
+            handleDp={e => this.handleDp(e)}
+          />
           <Module />
           <Singleton />
           <Prototype />
@@ -88,31 +96,50 @@ class MainApp extends React.Component {
           2 Structural Design Patterns <br />
           These patterns deal with object relationships. They ensure that if one
           part of a system changes, the entire system doesn’t need to change
-          along with it. The most popular patterns in this category are: <br />Adapter
-          Bridge<br /> Composite<br /> Decorator<br /> Facade<br /> Flyweight<br /> Proxy<br />
+          along with it. The most popular patterns in this category are: <br />
+          Adapter Bridge
+          <br /> Composite
+          <br /> Decorator
+          <br /> Facade
+          <br /> Flyweight
+          <br /> Proxy
+          <br />
         </div>
         <div className="blue">
           3 Behavioral Design Patterns <br />
           These types of patterns recognize, implement, and improve
           communication between disparate objects in a system. They help ensure
           that disparate parts of a system have synchronized information.
-          Popular examples of these patterns are: <br />Chain of responsibility
-          <br />Command Iterator<br /> Mediator <br />Memento <br />Observer <br />State Strategy <br />Visitor<br />
+          Popular examples of these patterns are: <br />
+          Chain of responsibility
+          <br />
+          Command Iterator
+          <br /> Mediator <br />
+          Memento <br />
+          Observer <br />
+          State Strategy <br />
+          Visitor
+          <br />
+          <Mediator />
           <Observer />
         </div>
         <div className="green">
           4 Concurrency Design Patterns
           <br /> These types of design patterns deal with multi-threaded
-          programming paradigms. Some of the popular ones are: <br />Active object<br />
+          programming paradigms. Some of the popular ones are: <br />
+          Active object
+          <br />
           Nuclear reaction Scheduler
-          
         </div>
 
         <div className="orange">
           5 Architectural Design Patterns <br />
           Design patterns which are used for architectural purposes. Some of the
-          most famous ones are: <br />MVC (Model-View-Controller) <br />MVP
-          (Model-View-Presenter)<br /> MVVM (Model-View-ViewModel)<br />
+          most famous ones are: <br />
+          MVC (Model-View-Controller) <br />
+          MVP (Model-View-Presenter)
+          <br /> MVVM (Model-View-ViewModel)
+          <br />
         </div>
       </div>
     );
